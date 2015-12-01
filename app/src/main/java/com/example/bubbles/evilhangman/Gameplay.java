f50c9068d57b1a5ba7005c1e7f694f97d82f6ac7
Victor <vdenhaan@gmail.com>
2015-12-02 00:20:03 +0100

    implemented part of Gameplay

--- app/src/main/java/com/example/bubbles/evilhangman/Gameplay.java
+++ app/src/main/java/com/example/bubbles/evilhangman/Gameplay.java
@@ -1,5 +1,98 @@
 package com.example.bubbles.evilhangman;
 
+import android.content.res.Resources;
+import android.view.View;
+
+import com.example.bubbles.evilhangman.GameplayActivity;
+
+import java.util.Random;
+
 public class Gameplay {
 
+    public String word_picked;
+
+    // picks a word randomly from the dictionary
+    public String random_word(){
+        Integer random_number = new Random().nextInt(GameplayActivity.words.length);
+        String[] array = GameplayActivity.words;
+        word_picked = array[random_number];
+        String the_word_was = word_picked;
+        return word_picked;
+    }
+
+    // puts up the correct amount of questionmarks
+    public String set_questionmarks(){
+        String temp = "";
+        for (Integer i = 0; i < word_picked.length(); i++) {
+            temp += "? ";
+        }
+        return temp;
+    }
+
+    public String letter_in_word_picked(String letter, String questionmarks) {
+        // checks if letter is in the_word
+        if (word_picked.contains(letter)) {
+            // changes the ?(s) to the correct letter
+            char[] qmarks = questionmarks.toCharArray();
+            char[] word = word_picked.toCharArray();
+            for (int i = 0; i < word.length; i++) {
+                if (word[i] == letter.toCharArray()[0]) {
+                    qmarks[i * 2] = letter.toCharArray()[0];
+                }
+            }
+            return qmarks.toString();
+        }
+        return questionmarks;
+    }
+                /*int location = picked_word.indexOf(first_letter);
+                String new_word = word.substring(0,(location * 2))+ first_letter + word.substring(location * 2 + 1);
+                the_word.setText(new_word);
+
+                // removes the letter from the computer's word
+                char[] picked_word_chars = picked_word.toCharArray();
+                picked_word_chars[location] = '?';
+                picked_word = String.valueOf(picked_word_chars);
+
+                // check if the entire word has been revealed
+                if(questionmarks.matches("^[\pL\pN]+$")) {
+                    guess_button.setVisibility(View.INVISIBLE);
+            }
+        }
+
+
+        // removes one guess chance if letter not in word
+        else {*/
+            // remove 1 from guesses left
+            // change picture accordingly
+
+            /*Integer amount = Integer.parseInt(guesses_left.getText().toString().substring(9, 10));
+            amount -= 1;
+            guesses_left.setText("You have " + amount + " wrong guesses left!");
+            switch (amount) {
+                case 5:
+                    hangman6.setVisibility(View.INVISIBLE);
+                    hangman5.setVisibility(View.VISIBLE);
+                    break;
+                case 4:
+                    hangman5.setVisibility(View.INVISIBLE);
+                    hangman4.setVisibility(View.VISIBLE);
+                    break;
+                case 3:
+                    hangman4.setVisibility(View.INVISIBLE);
+                    hangman3.setVisibility(View.VISIBLE);
+                    break;
+                case 2:
+                    hangman3.setVisibility(View.INVISIBLE);
+                    hangman2.setVisibility(View.VISIBLE);
+                    break;
+                case 1:
+                    hangman2.setVisibility(View.INVISIBLE);
+                    hangman1.setVisibility(View.VISIBLE);
+                    break;
+                case 0:
+                    hangman1.setVisibility(View.INVISIBLE);
+                    hangman0.setVisibility(View.VISIBLE);
+                    guess_button.setVisibility(View.INVISIBLE);
+                    break;
+            }*/
 }
--- app/src/main/java/com/example/bubbles/evilhangman/GameplayActivity.java
+++ app/src/main/java/com/example/bubbles/evilhangman/GameplayActivity.java
@@ -4,28 +4,111 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
-import java.util.Random;
+import com.example.bubbles.evilhangman.Views.CrayonButton;
+import com.example.bubbles.evilhangman.Views.CrayonTextView;
 
 public class GameplayActivity extends Activity {
+
+    static String[] words;
+    CrayonTextView questionmarks, letters_tried;
+    CrayonButton guess_button;
+    String word_picked, letter;
+    Gameplay gameplay;
+    EditText letter_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameplay);
+        gameplay = new Gameplay();
+        words = getResources().getStringArray(R.array.words_short);
 
+        initialise();
+        reset();
     }
 
-    // picks a word randomly from the dictionary
-    private void random_word(){
-        Integer random_number = new Random().nextInt(getResources().getStringArray(R.array.words_small).length);
-        String[] array = getResources().getStringArray(R.array.words_small);
-        String word_picked = array[random_number];
-        String the_word_was = word_picked;
+    private void initialise() {
+        guess_button = (CrayonButton) findViewById(R.id.guess_button);
+        questionmarks = (CrayonTextView) findViewById(R.id.questionmarks);
+        letter_input = (EditText) findViewById(R.id.letter_input);
+        letters_tried = (CrayonTextView) findViewById(R.id.letters_tried);
     }
 
     public void back_to_menu_button_click(View view) {
         Intent intent = new Intent(GameplayActivity.this, MenuActivity.class);
         startActivity(intent);
+    }
+
+    public void guess_button_click(View view) {
+        if (get_letter_input()){
+            String qmarks = questionmarks.getText().toString();
+            String temp = gameplay.letter_in_word_picked(letter, qmarks);
+            questionmarks.setText(temp);
+        }
+        letter_input.setText("");
+    }
+
+    public void new_word_button_click(View view) {
+        reset();
+    }
+
+    private void reset(){
+        word_picked = gameplay.random_word();
+        questionmarks.setText(gameplay.set_questionmarks());
+        set_questionmark_size();
+        letters_tried.setText(R.string.letters_tried);
+    }
+
+    private void set_questionmark_size(){
+        switch (word_picked.length()) {
+            default:
+                questionmarks.setTextSize(48);
+                break;
+            case 9:
+                questionmarks.setTextSize(44);
+                break;
+            case 10:
+                questionmarks.setTextSize(40);
+                break;
+            case 11:
+                questionmarks.setTextSize(36);
+                break;
+            case 12:
+                questionmarks.setTextSize(32);
+                break;
+            case 13:
+                questionmarks.setTextSize(30);
+                break;
+            case 14:
+                questionmarks.setTextSize(28);
+                break;
+            case 15:
+                questionmarks.setTextSize(26);
+                break;
+            case 16:
+                questionmarks.setTextSize(24);
+                break;
+        }
+    }
+
+    // returns the character input if it's a letter and not tried yet
+    private boolean get_letter_input() {
+        // takes the first character of user input (disregards if no input)
+        if (letter_input.getText().toString().length() != 0) {
+            String l = letter_input.getText().toString().substring(0, 1).toLowerCase();
+            // checks if the character is a letter
+            if (l.matches("[a-z]+")) {
+                // checks with already tried letters
+                String tried = letters_tried.getText().toString();
+                if (!tried.contains(l)){
+                    letters_tried.setText(tried + " " + l);
+                    letter = l;
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
